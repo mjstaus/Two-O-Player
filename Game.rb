5b0f23c6e2db329e6d14237c9c8ee979870e3837
Martha Staus <mjstaus@gmail.com>
2022-04-20 16:38:53 -0700

    FEATURE: Add functionality to game class

--- Game.rb
+++ Game.rb
@@ -4,6 +4,19 @@ require_relative "question"
 class Game
   attr_accessor :current_player
 
+  def start_game
+    while players_alive?
+      ask_question
+      if(players_alive?)
+        toggle_player
+        puts "--- NEW TURN ---"
+      end
+    end
+    game_over
+  end
+
+private
+
   def initialize
     @player1 = Player.new(1)
     @player2 = Player.new(2)
@@ -25,17 +38,29 @@ class Game
     input = $stdin.gets.chomp
     if question.is_correct(input) == false
       @current_player.lose_life
-      puts @current_player.lives
+      puts "Player #{@current_player.name}: Seriously? No."
+    else question.is_correct(input) == true
+      puts "Player #{@current_player.name}: Yes you got it!"
     end
+    puts "P1: #{@player1.lives}/3 vs P2: #{@player2.lives}/3"
   end
 
+  def players_alive? 
+    @player1.lives > 0 && @player2.lives > 0
+  end
 
-  
+  def game_over
+    toggle_player
+    puts "Player #{@current_player.name} wins with a score of #{@current_player.lives}/3"  
+    puts "--- GAME OVER ---"
+    puts "Goodbye!"
+  end
   
 end
 
-game = Game.new
-game.ask_question
+
+
+
 
 
 
